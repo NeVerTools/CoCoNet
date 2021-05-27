364e33f39aae6fdcb11d3a1c9b85d27486cb8fea
sdemarch <stefano.demarchi@edu.unige.it>
2021-05-27 10:19:19 +0200

    Update Convolutional node

--- coconet/core/controller/nodewrapper.py
+++ coconet/core/controller/nodewrapper.py
@@ -35,17 +35,17 @@ class NodeOps:
 
         """
 
-        if class_name == "ReLUNode":
+        if class_name == 'ReLUNode':
             node = ReLUNode(node_id,
                             in_dim)
-        elif class_name == "FullyConnectedNode":
+        elif class_name == 'FullyConnectedNode':
             node = FullyConnectedNode(node_id,
                                       in_dim,
                                       in_dim[-1],
                                       data["out_features"],
                                       Tensor((data["out_features"], in_dim[-1])),
                                       Tensor((data["out_features"],)))
-        elif class_name == "BatchNormNode":
+        elif class_name == 'BatchNormNode':
             node = BatchNormNode(node_id,
                                  in_dim, data["num_features"],
                                  Tensor((data["num_features"],)),
@@ -54,7 +54,7 @@ class NodeOps:
                                  data["running_var"], data["eps"],
                                  data["momentum"], data["affine"],
                                  data["track_running_stats"])
-        elif class_name == "AveragePoolNode":
+        elif class_name == 'AveragePoolNode':
             node = AveragePoolNode(node_id,
                                    in_dim,
                                    data["kernel_size"],
@@ -62,7 +62,7 @@ class NodeOps:
                                    data["padding"],
                                    data["ceil_mode"],
                                    data["count_include_pad"])
-        elif class_name == "ConvNode":
+        elif class_name == 'ConvNode':
             node = ConvNode(node_id,
                             in_dim,
                             data["in_channels"],
@@ -75,7 +75,7 @@ class NodeOps:
                             data["has_bias"],
                             data["bias"],
                             data["weight"])
-        elif class_name == "MaxPoolNode":
+        elif class_name == 'MaxPoolNode':
             node = MaxPoolNode(node_id,
                                in_dim,
                                data["kernel_size"],
@@ -84,30 +84,30 @@ class NodeOps:
                                data["dilation"],
                                data["ceil_mode"],
                                data["return_indices"])
-        elif class_name == "LRNNode":
+        elif class_name == 'LRNNode':
             node = LRNNode(node_id,
                            in_dim,
                            data["size"],
                            data["alpha"],
                            data["beta"],
                            data["k"])
-        elif class_name == "SoftMaxNode":
+        elif class_name == 'SoftMaxNode':
             node = SoftMaxNode(node_id,
                                in_dim,
                                data["axis"])
-        elif class_name == "UnsqueezeNode":
+        elif class_name == 'UnsqueezeNode':
             node = UnsqueezeNode(node_id,
                                  in_dim,
                                  data["axes"])
-        elif class_name == "FlattenNode":
+        elif class_name == 'FlattenNode':
             node = FlattenNode(node_id,
                                in_dim,
                                data["axis"])
-        elif class_name == "DropoutNode":
+        elif class_name == 'DropoutNode':
             node = DropoutNode(node_id,
                                in_dim,
                                data["p"])
-        elif class_name == "ReshapeNode":
+        elif class_name == 'ReshapeNode':
             node = ReshapeNode(node_id,
                                in_dim,
                                data["shape"])
@@ -138,7 +138,7 @@ class NodeOps:
         """
 
         if type(in_dim) != tuple and len(in_dim) < 0:
-            raise Exception(f"{LayerNode.__class__.__name__} - empty input.")
+            raise Exception(f'{LayerNode.__class__.__name__} - empty input.')
         else:
             out = ()
             if isinstance(node, ReLUNode):
@@ -156,8 +156,8 @@ class NodeOps:
                     out += (node.out_features,)
                     node.out_dim = out
                 else:
-                    raise Exception("FullyConnectedNode - Wrong input: last value of"
-                                    "input should be " + str(node.in_features))
+                    raise Exception('FullyConnectedNode - Wrong input: last value of'
+                                    f'input should be {node.in_features}')
 
             elif isinstance(node, BatchNormNode):
                 if len(in_dim) > 0:
@@ -183,7 +183,7 @@ class NodeOps:
                                                stride_size[i]) + 1,)
                     node.out_dim = out
                 else:
-                    raise Exception("AveragePoolNode - Wrong input size: should be greater than 1.")
+                    raise Exception('AveragePoolNode - Wrong input size: should be > 1')
 
             elif isinstance(node, ConvNode):
                 if len(in_dim) > 2 and 0 < in_dim[-3] == node.in_channels:
@@ -200,8 +200,8 @@ class NodeOps:
                                             (node.kernel_size[i] - 1) - 1) / stride_size[i]) + 1,)
                     node.out_dim = out
                 else:
-                    raise Exception("ConvNode - Wrong input size: should be greater than 2"
-                                    "and the third last element should be " + str(node.in_channels))
+                    raise Exception('ConvNode - Wrong input size: should be > 2'
+                                    f'and the third last element should be {node.in_channels}')
 
             elif isinstance(node, MaxPoolNode):
                 if len(in_dim) > 1:
@@ -222,21 +222,21 @@ class NodeOps:
                                     node.kernel_size[i] - 1) - 1) / stride_size[i])) + 1,)
                     node.out_dim = out
                 else:
-                    raise Exception("MaxPoolNode - Wrong input size: should be greater than 1.")
+                    raise Exception('MaxPoolNode - Wrong input size: should be > 1')
 
             elif isinstance(node, LRNNode):
                 if len(in_dim) >= 2:
                     node.in_dim = in_dim
                     node.out_dim = in_dim
                 else:
-                    raise Exception("LRNNode - Wrong input size: should be greater than 2.")
+                    raise Exception('LRNNode - Wrong input size: should be > 2')
 
             elif isinstance(node, SoftMaxNode):
                 if len(in_dim) >= 0:
                     node.in_dim = in_dim
                     node.out_dim = in_dim
                 else:
-                    raise Exception("SoftMaxNode - Wrong input.")
+                    raise Exception('SoftMaxNode - Wrong input.')
 
             elif isinstance(node, UnsqueezeNode):
                 if len(in_dim) > 0:
@@ -252,21 +252,21 @@ class NodeOps:
                                 out.insert((ax[i] + 1), 1)
                                 node.out_dim = tuple(out)
                 else:
-                    raise Exception("SoftMaxNode - Wrong input size: empty")
+                    raise Exception('SoftMaxNode - Wrong input size: empty.')
 
             elif isinstance(node, FlattenNode):
                 if len(in_dim) > 0:
                     node.in_dim = in_dim
                     # TODO HOW TO COMPUTE OUT_DIM?
                 else:
-                    raise Exception("FlattenNode - Wrong input size: empty")
+                    raise Exception('FlattenNode - Wrong input size: empty.')
 
             elif isinstance(node, DropoutNode):
                 if len(in_dim) > 0:
                     node.in_dim = in_dim
                     node.out_dim = in_dim
                 else:
-                    raise Exception("DropoutNode - Wrong input size: empty")
+                    raise Exception('DropoutNode - Wrong input size: empty.')
 
             elif isinstance(node, ReshapeNode):
                 if len(in_dim) > 0:
@@ -279,7 +279,7 @@ class NodeOps:
                         out += (out_dim.shape[i],)
                     node.out_dim = out
                 else:
-                    raise Exception("ReshapeNode - Wrong input size: empty")
+                    raise Exception('ReshapeNode - Wrong input size: empty.')
 
             return node
 
@@ -308,12 +308,12 @@ class NodeOps:
             if type(data["in_features"]) == int and data["in_features"] > 0:
                 node.in_features = data["in_features"]
             else:
-                raise Exception('FullyConnectedNode - Wrong ''in_features'' value, should be int and > 0')
+                raise Exception('FullyConnectedNode - Wrong "in_features" value, should be int and > 0')
 
             if type(data["out_features"]) == int and data["out_features"] > 0:
                 node.out_features = data["out_features"]
             else:
-                raise Exception('FullyConnectedNode - Wrong ''out_features'' value, should be int and > 0')
+                raise Exception('FullyConnectedNode - Wrong "out_features" value, should be int and > 0')
 
             weight = data["weight"]
             if weight is None:
@@ -321,7 +321,7 @@ class NodeOps:
             if weight.shape == (node.out_features, node.in_features):
                 node.weight = weight
             else:
-                raise Exception('FullyConnectedNode - Wrong weight dimension')
+                raise Exception('FullyConnectedNode - Wrong weight dimension.')
 
             bias = data["bias"]
             if bias is None:
@@ -329,7 +329,7 @@ class NodeOps:
             if bias.shape == (node.out_features,):
                 node.bias = bias
             else:
-                raise Exception('FullyConnectedNode - Wrong bias dimension')
+                raise Exception('FullyConnectedNode - Wrong bias dimension.')
 
         elif isinstance(node, BatchNormNode):
             for dim in range(len(node.in_dim)):
@@ -338,7 +338,7 @@ class NodeOps:
 
             if node.num_features is None:
                 raise Exception(f'BatchNormNode - Wrong input, num_features = {node.num_features}'
-                                f'not feasible with input shape = {node.in_dim} ')
+                                f'not feasible with input shape = {node.in_dim}')
 
             if data["track_running_stats"] and data["running_mean"] is None and data["running_var"] is None:
                 data["running_mean"] = np.ones(node.num_features)
@@ -350,7 +350,7 @@ class NodeOps:
             if weight.shape == (node.num_features,):
                 node.weight = weight
             else:
-                raise Exception('BatchNormNode - Wrong weight dimension')
+                raise Exception('BatchNormNode - Wrong weight dimension.')
 
             bias = data["bias"]
             if bias is None:
@@ -358,7 +358,7 @@ class NodeOps:
             if bias.shape == (node.num_features,):
                 node.bias = bias
             else:
-                raise Exception('BatchNormNode - Wrong bias dimension')
+                raise Exception('BatchNormNode - Wrong bias dimension.')
 
             node.running_mean = data["running_mean"]
             node.running_var = data["running_var"]
@@ -408,34 +408,110 @@ class NodeOps:
                 if padding[0] <= half[0] and padding[1] <= half[1]:
                     node.padding = padding
                 else:
-                    raise Exception(f'AveragePoolNode - padding should be smaller than half of kernel size,' 
-                                    f'but got padW = {padding[1]}, padH = {padding[0]},' 
+                    raise Exception(f'AveragePoolNode - padding should be smaller than half of kernel size,'
+                                    f'but got padW = {padding[1]}, padH = {padding[0]},'
                                     f'kW = {kernel[1]}, kH = {kernel[0]}')
             else:
-                raise Exception('AveragePoolNode - padding type must be int or tuple')
+                raise Exception('AveragePoolNode - padding type must be int or tuple.')
 
             count_include_pad = data["count_include_pad"]
             if type(count_include_pad) is bool:
                 node.count_include_pad = count_include_pad  # Do not modify shape
             else:
-                raise Exception('AveragePoolNode - count_include_pad must be bool')
+                raise Exception('AveragePoolNode - count_include_pad must be bool.')
 
             ceil_mode = data["ceil_mode"]
             if type(ceil_mode) is bool:
                 node.ceil_mode = ceil_mode
             else:
-                raise Exception('AveragePoolNode - ceil_mode must be bool')
+                raise Exception('AveragePoolNode - ceil_mode must be bool.')
 
         elif isinstance(node, ConvNode):
-            node.update(node.in_dim,
-                        data["in_channels"],
-                        data["out_channels"],
-                        data["kernel_size"],
-                        data["dilation"],
-                        data["groups"],
-                        Tensor((data["num_features"],)),
-                        data["padding"],
-                        data["stride_size"])
+            in_channels = data["in_channels"]
+            if type(in_channels) == int and in_channels > 0:
+                node.in_channels = in_channels
+            else:
+                raise Exception('ConvNode - in_channels must be int and > 0')
+
+            out_channels = data["out_channels"]
+            if type(out_channels) == int and out_channels > 0:
+                node.out_channels = out_channels
+            else:
+                raise Exception('ConvNode - Wrong out_channel value, must be int > 0')
+
+            bias = data["bias"]
+            if bias is None:
+                bias = np.random.normal(size=[out_channels])
+            if type(bias) == Tensor and bias.size == out_channels:
+                node.bias = bias
+            else:
+                raise Exception(f'ConvNode - Wrong bias value, it should have shape of {out_channels}')
+
+            weight = data["weight"]
+            size = (out_channels, in_channels)
+            size += data["kernel_size"]
+            if weight is None:
+                weight = np.random.normal(size=size)
+            if type(weight) == Tensor and weight.shape == size:
+                node.weight = weight
+            else:
+                raise Exception(f'ConvNode - Wrong weight value, it should have shape = {size}')
+
+            kernel = data["kernel_size"]
+            if type(kernel) == tuple:
+                if len(kernel) == 2 and kernel[0] > 0 and kernel[1] > 0:
+                    node.kernel_size = kernel
+                elif len(kernel) == 1 and kernel[0] > 0:
+                    kernel = (kernel[0], kernel[0])
+                    node.kernel_size = kernel
+                else:
+                    raise Exception('ConvNode - Wrong kernel size value, must be > 0')
+            else:
+                raise Exception('ConvNode - Wrong kernel size value, must be > 0')
+
+            stride = data["stride"]
+            if type(stride) == tuple:
+                if len(stride) == 2 and stride[0] > 0 and stride[1] > 0:
+                    node.stride = stride
+                elif len(stride) == 1 and stride[0] > 0:
+                    stride = (stride[0], stride[0])
+                    node.stride = stride
+                else:
+                    raise Exception('ConvNode - Wrong stride value, must be > 0')
+            else:
+                raise Exception('ConvNode - Wrong stride value, must be > 0')
+
+            padding = data["padding"]
+            if type(padding) == tuple:
+                if len(padding) == 2 and padding[0] >= 0 and padding[1] >= 0:
+                    node.padding = padding
+                elif len(padding) == 1 and padding[0] >= 0:
+                    padding = (padding[0], padding[0])
+                    node.padding = padding
+                else:
+                    raise Exception('ConvNode - Wrong padding value, must be >= 0')
+            else:
+                raise Exception('ConvNode - Wrong padding value, must be >= 0')
+
+            dilation = data["dilation"]
+            if type(dilation) == tuple:
+                if len(dilation) == 2 and dilation[0] >= 0 and dilation[1] >= 0:
+                    node.dilation = dilation
+                elif len(dilation) == 1 and dilation[0] >= 0:
+                    dilation = (dilation[0], dilation[0])
+                    node.dilation = dilation
+                else:
+                    raise Exception('ConvNode - Wrong dilation value, must be >= 0')
+            else:
+                raise Exception('ConvNode - Wrong dilation value, must be >= 0')
+
+            groups = data["groups"]
+            if groups > 0 and in_channels % groups == 0 and out_channels % groups == 0:
+                node.groups = groups
+            else:
+                raise Exception('ConvNode - Wrong groups value,'
+                                'in_channels and out_channels must be divisible by groups')
+
         elif isinstance(node, MaxPoolNode):
             node.update(node.in_dim,
                         data["kernel_size"],
